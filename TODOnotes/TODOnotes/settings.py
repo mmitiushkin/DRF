e42f3843fd213d6e01cea11816fac4d696c64e0c
mmitiushkin <mmitiushkin@gmail.com>
2021-12-13 18:00:58 +0300

    lesson-1 installed DRF created model User, serializer

--- TODOnotes/TODOnotes/settings.py
+++ TODOnotes/TODOnotes/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
 
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
---
+++ TODOnotes/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f'{self.username}'
+
