d58b72e0d5579645689f865cc1b711706bb66986
mmitiushkin <mmitiushkin@gmail.com>
2021-12-14 18:41:39 +0300

    lesson-2

--- TODOnotes/TODOnotes/settings.py
+++ TODOnotes/TODOnotes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'users',
 ]
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'TODOnotes.urls'
@@ -125,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
+
---
+++ TODOnotes/frontend/src/components/UserList.js
@@ -0,0 +1,28 @@
+const UserItem = ({user}) => {
+    return(
+        <tr>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last name
+            </th>
+            <th>
+                email
+            </th>
+            {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList;
